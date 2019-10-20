241a7adc5c01fae7999c1882e2dae4e4d23c38dd
Pedro Huertas <pmh242@gmail.com>
2019-10-20 08:11:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+end
+
+def any_phrase
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+end
